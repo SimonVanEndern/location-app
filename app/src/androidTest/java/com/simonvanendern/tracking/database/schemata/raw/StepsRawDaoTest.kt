af972b4f1c1ad70d9115d16473bac0a7a78e3075
Simon van Endern <simon.koenigx@gmail.com>
2019-05-19 12:23:25 +0200

    Added missing unit tests for stepsRawDao

--- app/src/androidTest/java/com/simonvanendern/tracking/database/schemata/raw/StepsRawDaoTest.kt
+++ app/src/androidTest/java/com/simonvanendern/tracking/database/schemata/raw/StepsRawDaoTest.kt
@@ -2,9 +2,8 @@ package com.simonvanendern.tracking.database.schemata.raw
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.simonvanendern.tracking.database.DatabaseTest
-import com.simonvanendern.tracking.database.schemata.raw.StepsRaw
-import com.simonvanendern.tracking.database.schemata.raw.StepsRawDao
-import org.junit.Assert
+import com.simonvanendern.tracking.repository.StepsRepository
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,7 +29,7 @@ class StepsRawDaoTest : DatabaseTest() {
         val stepsObject = StepsRaw(day.time, day, steps, false)
         stepsRawDao.insert(stepsObject)
         val result = stepsRawDao.getSteps(day)
-        Assert.assertEquals(steps, result)
+        assertEquals(steps, result)
     }
 
     @Test
@@ -42,10 +41,100 @@ class StepsRawDaoTest : DatabaseTest() {
         val stepsObject1 = StepsRaw(day.time, day, steps1, false)
         stepsRawDao.insert(stepsObject1)
         var result = stepsRawDao.getSteps(day)
-        Assert.assertEquals(steps1, result)
+        assertEquals(steps1, result)
         val stepsObject2 = StepsRaw(day.time, day, steps2, false)
         stepsRawDao.insert(stepsObject2)
         result = stepsRawDao.getSteps(day)
-        Assert.assertEquals(steps2, result)
+        assertEquals(steps2, result)
+    }
+
+    @Test
+    fun testGetLastTimestampOnEmptyTable() {
+        val result = stepsRawDao.getLastTimestamp()
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun testGetLastTimestamp() {
+        val dayFraction = "2019-01-"
+
+        for (i in 11..30) {
+            stepsRawDao.insert(
+                StepsRaw(
+                    formatter.parse(dayFraction + i).time,
+                    formatter.parse(dayFraction + i),
+                    300,
+                    false
+                )
+            )
+        }
+
+        val result = stepsRawDao.getLastTimestamp()
+
+        assertEquals(
+            formatter.parse(dayFraction + 30).time,
+            result
+        )
+    }
+
+    @Test
+    fun computeNewSteps() {
+        stepsRawDao = getDb().stepsRawDao()
+
+        val day1 = "2019-01-02"
+        val day2 = "2019-01-03"
+        val day3 = "2019-01-04"
+
+        val stepsRaw = listOf(
+            StepsRaw(
+                formatter.parse(day1).time,
+                formatter.parse(day1),
+                50,
+                true
+            ),
+            StepsRaw(
+                formatter.parse(day1).time + 10000,
+                formatter.parse(day1),
+                200,
+                false
+            ),
+            StepsRaw(
+                formatter.parse(day1).time + 30000,
+                formatter.parse(day1),
+                20,
+                false
+            ),
+            StepsRaw(
+                formatter.parse(day2).time + 200,
+                formatter.parse(day2),
+                40,
+                false
+            ),
+            StepsRaw(
+                formatter.parse(day3).time + 100,
+                formatter.parse(day3),
+                10,
+                false
+            )
+        )
+
+        stepsRawDao.insertAll(stepsRaw)
+
+        val result = stepsRawDao.computeNewSteps()
+
+        assertEquals(4, result.size)
+
+        assertEquals(150, result[0].steps)
+        assertEquals(formatter.parse(day1), result[0].day)
+
+        assertEquals(20, result[1].steps)
+        assertEquals(formatter.parse(day1), result[1].day)
+
+        assertEquals(20, result[2].steps)
+        assertEquals(formatter.parse(day1), result[2].day)
+
+        assertEquals(10, result[3].steps)
+        assertEquals(formatter.parse(day2), result[3].day)
     }
 }
--- app/src/main/java/com/simonvanendern/tracking/database/schemata/raw/StepsRawDao.kt
+++ app/src/main/java/com/simonvanendern/tracking/database/schemata/raw/StepsRawDao.kt
@@ -1,6 +1,5 @@
 package com.simonvanendern.tracking.database.schemata.raw
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
@@ -14,27 +13,26 @@ interface StepsRawDao {
     @Insert(onConflict = REPLACE)
     fun insert(stepStatistic: StepsRaw)
 
+    @Insert(onConflict = REPLACE)
+    fun insertAll(steps: List<StepsRaw>)
+
     @Query("SELECT * FROM step_counter_table")
-    fun getAll () : List<StepsRaw>
+    fun getAll(): List<StepsRaw>
 
     @Query("SELECT steps FROM step_counter_table WHERE day = :day LIMIT 1")
     fun getSteps(day: Date): Int
 
-    // For testing
-    @Query("SELECT steps FROM step_counter_table ORDER BY day DESC LIMIT 10")
-    fun get10RecentSteps (): LiveData<List<Int>>
-
     @Query("SELECT MAX(timestamp) FROM step_counter_table")
-    fun getLastTimestamp() : Long
+    fun getLastTimestamp(): Long
 
-//    @Query("""SELECT *
-//                    FROM step_counter_table
-//                    WHERE timestamp =
-//                    (SELECT MIN(timestamp) FROM step_counter_table)
-//                    LIMIT 1""")
-//    fun getFirstEntry() : StepsRaw
+    @Query(
+        """UPDATE step_counter_table SET processed = 1
+                WHERE timestamp <= :lastTimestamp"""
+    )
+    fun setProcessed(lastTimestamp: Long)
 
-    @Query("""
+    @Query(
+        """
             SELECT s1.timestamp, s1.day, s2.steps - s1.steps AS steps
             FROM step_counter_table AS s1, step_counter_table AS s2
             WHERE s2.processed = 0
@@ -48,12 +46,7 @@ interface StepsRawDao {
             AND s2.steps < s1.steps
             AND s2.timestamp =
             (SELECT MIN(timestamp) FROM step_counter_table WHERE timestamp > s1.timestamp);
-        """)
-    fun computeNewSteps() : List<Steps>
-
-    @Query(
-        """UPDATE step_counter_table SET processed = 1
-                WHERE timestamp <= :lastTimestamp"""
+        """
     )
-    fun setProcessed(lastTimestamp: Long)
+    fun computeNewSteps(): List<Steps>
 }
