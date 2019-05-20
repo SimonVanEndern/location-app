dc5cf4288f246e4f60888856af5502b54c9c27b1
Simon van Endern <simon.koenigx@gmail.com>
2019-05-20 14:36:25 +0200

    Added Interceptor for decrypting the response body before parsing it into a Java Object

--- app/src/main/java/com/simonvanendern/tracking/ApplicationComponent.kt
+++ app/src/main/java/com/simonvanendern/tracking/ApplicationComponent.kt
@@ -5,8 +5,6 @@ import com.simonvanendern.tracking.aggregation.ServerRequestHandler
 import com.simonvanendern.tracking.logging.ActivityRecognitionReceiver
 import com.simonvanendern.tracking.logging.LocationReceiver
 import com.simonvanendern.tracking.logging.StepsLogger
-import com.simonvanendern.tracking.repository.ActivityRepository
-import com.simonvanendern.tracking.repository.RequestRepository
 import dagger.Component
 import javax.inject.Singleton
 
@@ -15,8 +13,8 @@ import javax.inject.Singleton
 interface ApplicationComponent {
     fun inject(application: MyApplication)
     fun inject(databaseAggregator: DatabaseAggregator)
-    fun inject(activityRecognitionReceiver : ActivityRecognitionReceiver)
-    fun inject(LocationReceiver : LocationReceiver)
+    fun inject(activityRecognitionReceiver: ActivityRecognitionReceiver)
+    fun inject(LocationReceiver: LocationReceiver)
     fun inject(stepsLogger: StepsLogger)
     fun inject(allDataViewModel: AllDataViewModel)
     fun inject(serverRequestHandler: ServerRequestHandler)
--- app/src/main/java/com/simonvanendern/tracking/ApplicationModule.kt
+++ app/src/main/java/com/simonvanendern/tracking/ApplicationModule.kt
@@ -1,8 +1,10 @@
 package com.simonvanendern.tracking
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Room
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
 import com.simonvanendern.tracking.aggregation.RequestExecuter
 import com.simonvanendern.tracking.communication.WebService
 import com.simonvanendern.tracking.database.TrackingDatabase
@@ -10,6 +12,12 @@ import com.simonvanendern.tracking.repository.ActivityRepository
 import com.simonvanendern.tracking.repository.RequestRepository
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.ResponseBody
+import org.json.JSONArray
+import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -20,6 +28,28 @@ class ApplicationModule(private val application: Context) {
     @Singleton
     @Provides
     fun provideWebservice(): WebService {
+        val interceptor = Interceptor { chain ->
+            {
+                val request:Request = chain.request()
+                val response = chain.proceed(request)
+                val raw = response.body()?.string()
+                println(raw)
+                Log.e("INTERCEPTOR", raw)
+                val json = JSONArray(raw)
+                for (i in json.length() - 1 downTo 0) {
+                    json.getJSONObject(i).put("test", "test")
+                }
+                val newBody = ResponseBody.create(
+                    response.body()?.contentType(),
+                    json.toString()
+                )
+                response.newBuilder().body(newBody).build()
+            }()
+        }
+        val clientBuilder = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val gson = GsonBuilder()
             .setDateFormat("yyyy-MM-dd")
             .create()
@@ -27,6 +57,7 @@ class ApplicationModule(private val application: Context) {
             .baseUrl("https://privacy-research-proud-platypus.eu-gb.mybluemix.net/")
 //            .baseUrl("http://localhost:8888/")
             .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(clientBuilder)
             .build()
             .create(WebService::class.java)
     }
