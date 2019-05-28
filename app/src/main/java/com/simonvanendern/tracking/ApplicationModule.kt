386a6ce9400f0d6e4c7eb0c85d61e79212b6906f
Simon van Endern <simon.koenigx@gmail.com>
2019-05-28 12:12:02 +0200

    Decryption working

--- app/src/main/java/com/simonvanendern/tracking/ApplicationModule.kt
+++ app/src/main/java/com/simonvanendern/tracking/ApplicationModule.kt
@@ -23,8 +23,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.security.KeyFactory
 import java.security.PrivateKey
+import java.security.SecureRandom
 import java.security.spec.PKCS8EncodedKeySpec
 import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 import javax.inject.Singleton
 
 @Module
@@ -41,6 +45,11 @@ class ApplicationModule(private val application: Context) {
             return keyFactory.generatePrivate(spec)
         }
 
+        fun ByteArray.toAESKey() : SecretKey {
+            val spec = SecretKeySpec(this, "AES")
+            return spec
+        }
+
         val interceptor = Interceptor { chain ->
             {
                 val request: Request = chain.request()
@@ -53,9 +62,23 @@ class ApplicationModule(private val application: Context) {
                 cipher.init(Cipher.DECRYPT_MODE, key)
 
                 try {
-                    val json = JSONObject(raw)
-                    val value = json.getString("encryptedRequest")
-                    val decrypted = cipher.doFinal(Base64.decode(value.toByteArray(), 0))
+                    val json = JSONArray(raw)
+                    for (i in json.length() - 1 downTo 0) {
+                        val encryptedKey = json.getJSONObject(i).getString("encryptionKey")
+                        val key = cipher.doFinal(Base64.decode(encryptedKey.toByteArray(), 0))
+                        val sync = json.getJSONObject(i).getString("sync")
+                        val buffer = Base64.decode(sync, 0)
+                        val encryptedRequest = Base64.decode(json.getJSONObject(i).getString("encryptedRequest"), 0)
+                        val synchronousCipher = Cipher.getInstance("AES/CBC/PKCS7PADDING")
+                        val iv = json.getJSONObject(i).getString("iv")
+                        synchronousCipher.init(Cipher.DECRYPT_MODE, key.toAESKey(), IvParameterSpec(Base64.decode(iv, 0)))
+                        val plainText = String(synchronousCipher.doFinal(encryptedRequest))
+                        val newBody = ResponseBody.create(
+                            response.body()?.contentType(),
+                            json.toString()
+                        )
+                        response.newBuilder().body(newBody).build()
+                    }
                 } catch (e : JSONException) {
                     val newBody = ResponseBody.create(
                         response.body()?.contentType(),
